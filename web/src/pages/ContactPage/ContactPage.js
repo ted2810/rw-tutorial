600c055aec92e6b7ad7aba60364b5896ad594211
Ted Stoychev <tedstoychev@gmail.com>
2020-03-28 17:15:58 +0200

    12.3. Introducing Form Helpers
    https://redwoodjs.com/tutorial/everyone-s-favorite-thing-to-build-forms#introducing-form-helpers

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,7 +1,15 @@
+import { Form, TextField, Submit } from '@redwoodjs/web'
 import BlogLayout from 'src/layouts/BlogLayout'
 
 const ContactPage = (props) => {
-  return <BlogLayout></BlogLayout>
+  return (
+    <BlogLayout>
+      <Form>
+        <TextField name="input" />
+        <Submit>Save</Submit>
+      </Form>
+    </BlogLayout>
+  )
 }
 
 export default ContactPage
